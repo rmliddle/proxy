73e94b89d8e300b9d3eb5a256609483b90726b57
Ryan <ryan@Ryans-MacBook-Air.local>
2026-05-09 09:06:52 +1000

    update page size

--- src/types.rs
+++ src/types.rs
@@ -2,7 +2,7 @@ use axum::{http::StatusCode, response::IntoResponse, Json};
 use serde::{Deserialize, Serialize};
 use tracing::warn;
 
-pub static PAGE_SIZE: usize = 1000;
+pub static PAGE_SIZE: usize = 100;
 
 #[derive(Clone, Debug)]
 pub enum DatabaseOperation {
